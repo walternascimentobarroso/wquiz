6cf1a9341ae12d78aeaa79a6515d94b571da7b27
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2016-05-10 00:25:28 -0400

    [ADJUST] Complementando a base de conhecimento
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- Programas/conhecimento.pl
+++ Programas/conhecimento.pl
@@ -13,6 +13,27 @@ conhecimento('qual e a capital do acre',['Rio Branco']).
 conhecimento('qual e a capital do para',['Belém']).
 conhecimento('qual e a capital de goias',['Goiânia']).
 
+%extremos
+conhecimento('qual o ponto do monte caburai, roraima',['Norte']).
+conhecimento('qual o ponto do chui, rio grande do sul', ['Sul']).
+conhecimento('qual o Ponta do seixas, em cabo branco',['Leste']).
+conhecimento('qual o nascente do rio moa, na serra de contamana ', ['Oeste']).
+conhecimento('qual o ponto mais alto do brasil', ['Pico da Neblina']).
+
+%relevos
+conhecimento('sao formas de relevo elevadas, com altitudes superiores a 300 metros',['Planaltos']).
+conhecimento('sao areas rebaixadas em consequencia da erosao, que se formam entre as bacias sedimentares e os escudos cristalinos', ['Depressões']).
+conhecimento('sao unidades de relevo geologicamente muito recentes',['Planícies']).
+conhecimento('caracterizam-se pela formacao de escarpas em areas de fronteira com as depressoes',['Planaltos']).
+conhecimento('sua formacao ocorre em virtude da sucessiva deposicao de material de origem marinha',['Planícies']).
+
+%vegetações
+conhecimento('milhares de especies vegetais nao perde suas folhas no outono ou seja esta sempre verde e dividida em 3 tipos de matas: igapo varzea terra firme',['Floresta Amazonica']).
+conhecimento('e menos densa que a floresta amazonica quase 100% dela ja foi destruida porem antes podiamos encontrar o pau-brasil cedro peroba e o jacaranda', ['Mata Atlantica']).
+conhecimento('vegetacao tipica do clima semi-arido do sertao nordestino vegetacao pobre com plantas que sao adaptadas a aridez sao as chamadas plantas xerofilas',['Caatinga']).
+conhecimento('corresponde as areas de clima subtropical e uma mata homogenea pois ha o predominio de pinheiros erva-mate imbuia canela cedros e ipes',['Mata de Araucaria']).
+conhecimento('tipica da regiao centro-oeste do brasil e formada por plantas tropofilas ou seja plantas adaptadas a uma estacao seca e outra umida',['Cerrado']).
+
 %conhecimento basico
 conhecimento('oi', 
 	['Ola!',
--- Programas/extremo.pl
+++ Programas/extremo.pl
@@ -5,4 +5,4 @@ extremo(1,'Qual o ponto do Monte Caburaí, Roraima','Norte').
 extremo(2,'Qual o ponto do Chuí, Rio Grande do Sul', 'Sul').
 extremo(3,'Qual o Ponta do Seixas, em Cabo Branco','Leste').
 extremo(4,'Qual o Nascente do rio Moa, na serra de Contamana ','Oeste').
-extremo(5,'Ponto mais alto do brasil','Pico da Neblina').
+extremo(5,'Qual o Ponto mais alto do brasil','Pico da Neblina').
