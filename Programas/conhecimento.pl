ee95663404d40a397d162791bb3dfe3f8b7f6663
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2016-05-09 20:11:55 -0400

    [FIX&ADD] base de conhecimento sobre capitais, e correção de caracteres especiais e acentuação
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- Programas/conhecimento.pl
+++ Programas/conhecimento.pl
@@ -1,10 +1,17 @@
 :- dynamic(conhecimento/2).
-%clima
-conhecimento('qual e o clima predominante na maior parte do brasil?',['Clima Tropical']).
-conhecimento('Qual e o clima quente mais proximo do arido',['Clima Semi-Árido']).
-conhecimento('qual clima abrange a porcao do territorio brasileiro ao sul do tropico de capricornio?',['Clima Subtropical']).
-conhecimento('qual e o clima que compreende a amazonia brasileira?',['Clima Equatorial']).
-conhecimento('qual clima compreende as proximidades do litoral desde o rio grande do norte ate a parte setentrional do estado de sao paulo?',['Clima litoraneo umido']).
+%climas
+conhecimento('qual e o clima predominante na maior parte do brasil',['Clima Tropical']).
+conhecimento('qual e o clima quente mais proximo do arido',['Clima Semi-Árido']).
+conhecimento('qual clima abrange a porcao do territorio brasileiro ao sul do tropico de capricornio',['Clima Subtropical']).
+conhecimento('qual e o clima que compreende a amazonia brasileira',['Clima Equatorial']).
+conhecimento('qual clima compreende as proximidades do litoral desde o rio grande do norte ate a parte setentrional do estado de sao paulo',['Clima litoraneo umido']).
+
+%capitais
+conhecimento('qual e a capital de roraima',['Boa Vista']).
+conhecimento('qual e a capital do amazonas',['Manaus']).
+conhecimento('qual e a capital do acre',['Rio Branco']).
+conhecimento('qual e a capital do para',['Belém']).
+conhecimento('qual e a capital de goias',['Goiânia']).
 
 %conhecimento basico
 conhecimento('oi', 
@@ -15,17 +22,14 @@ conhecimento('como voce se chama?', ['Meu nome é Professor SIEGE.']).
 
 conhecimento('qual o seu nome?', ['Meu nome é Professor SIEGE.']).
 
-conhecimento('tudo bem?', 
+conhecimento('tudo bem', 
 	['Tudo otimo!',
  	 'Tudo bem, e você?']).
 
-conhecimento('quem e voce?', 
+conhecimento('quem e voce', 
 	['Sou um robo.']).
 
 conhecimento(bye, 
 	['Ate mais! Foi otimo conversar com voce']).
-
-
-
 
 conhecimento(_, []).
--- js/chatterbot.js
+++ js/chatterbot.js
@@ -14,7 +14,6 @@ function ajax(fn, dados) {
 $(window).load(function () {
     $messages.mCustomScrollbar();
 });
-
 function updateScrollbar() {
     $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
         scrollInertia: 10,
@@ -30,6 +29,38 @@ function setDate() {
     }
 }
 
+//function removeAcento(strToReplace) {
+//    var str_acento = "áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ";
+//    var str_sem_acento = "aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC";
+//    var nova = "";
+//    for (var i = 0; i < strToReplace.length; i++) {
+//        if (str_acento.indexOf(strToReplace.charAt(i)) != -1) {
+//            nova += str_sem_acento.substr(str_acento.search(strToReplace.substr(i, 1)), 1);
+//        } else {
+//            nova += strToReplace.substr(i, 1);
+//        }
+//    }
+//    return nova;
+//}
+
+function removeAcento(s) {
+    var map = {"â": "a", "Â": "A", "à": "a", "À": "A", "á": "a", "Á": "A", "ã": "a", "Ã": "A", "ê": "e", "Ê": "E", "è": "e", "È": "E", "é": "e", "É": "E", "î": "i", "Î": "I", "ì": "i", "Ì": "I", "í": "i", "Í": "I", "õ": "o", "Õ": "O", "ô": "o", "Ô": "O", "ò": "o", "Ò": "O", "ó": "o", "Ó": "O", "ü": "u", "Ü": "U", "û": "u", "Û": "U", "ú": "u", "Ú": "U", "ù": "u", "Ù": "U", "ç": "c", "Ç": "C"};
+    return s.replace(/[\W\[\] ]/g, function (a) {
+        return map[a] || a;
+    });
+}
+
+function trim(str) {
+    return str.replace(/^\s+|\s+$/g, "");
+}
+
+function fixString(word) {
+    var newWord = removeAcento(word);
+    newWord = newWord.replace(/[\\\^\$\*\+\?\!\,\.\(\)\|\{\}\[\]]/g, '');
+    return trim(newWord);
+}
+
+
 function insertMessage() {
     msg = $('.message-input').val();
     if ($.trim(msg) == '') {
@@ -39,27 +70,24 @@ function insertMessage() {
     setDate();
     $('.message-input').val(null);
     updateScrollbar();
-    ajax(retorno, {answer: msg.toLowerCase()});
+    ajax(retorno, {answer: fixString(msg.toLowerCase())});
 }
 
 $('.message-submit').click(function () {
     insertMessage();
 });
-
 $(window).on('keydown', function (e) {
     if (e.which == 13) {
         insertMessage();
         return false;
     }
 });
-
 function retorno(retorno) {
     if ($('.message-input').val() != '') {
         return false;
     }
     $('<div class="message loading new"><figure class="avatar"><img src="img/mapabrasil.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
     updateScrollbar();
-
     setTimeout(function () {
         $('.message.loading').remove();
         $('<div class="message new"><figure class="avatar"><img src="img/mapabrasil.png" /></figure>' + retorno + '</div>').appendTo($('.mCSB_container')).addClass('new');
