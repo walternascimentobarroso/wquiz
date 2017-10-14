3259fa137f573bf2c37ad4535cc01dd498cc1710
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2017-10-13 21:24:48 -0400

    [ADD] Adicionando firebase
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,19 +1,21 @@
-google.charts.load('current', {
-    'packages': ['corechart']
-});
-google.charts.setOnLoadCallback(drawChart);
+$(document).ready(function () {
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['Certas', 9],
-        ['Erradas', 4]
-    ]);
-    var options = {
-        title: 'Respostas',
-        pieHole: 0.4,
-        is3D: true,
-    };
-    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-    chart.draw(data, options);
-}
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Certas', 9],
+            ['Erradas', 4]
+        ]);
+        var options = {
+            title: 'Respostas',
+            pieHole: 0.4,
+            is3D: true,
+        };
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+    }
+});
