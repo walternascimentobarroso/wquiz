6359f2f90fc05749b095e0226d1aecd759f7df10
Walter Nascimento <walter.nascimento@w3bsoftwares.com>
2017-10-23 09:15:14 -0400

    [CLEAN] Removendo backup do branch master
    Signed-off-by: Walter Nascimento <walter.nascimento@w3bsoftwares.com>

--- backup/Programas/capital.pl
+++
@@ -1,7 +0,0 @@
-:- dynamic(content/7).
-
-capital(1,'Qual é a capital de Roraima','Boa Vista').
-capital(2,'Qual é a capital do Amazonas','Manaus').
-capital(3,'Qual é a capital do Acre','Rio Branco').
-capital(4,'Qual é a capital do Pará','Belem').
-capital(5,'Qual é a capital de Goiás','Goiânia').
